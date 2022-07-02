864d210327d26c6005457c69bd284432da02f7f4
0x07H <30420689+0x07H@users.noreply.github.com>
2022-07-02 12:18:04 +0300

    wokring commit

--- src/main/java/gr/upatras/Library/LibraryApplication.java
+++ src/main/java/gr/upatras/Library/LibraryApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
-
 	public static void main(String[] args) {
-		//SpringApplication.run(LibraryApplication.class, args);
-		Book  book1 = new Book(7, "123-123-123-123", "Title7", "Author2", 80, 5);
-		Student mitsos = new Student(1234,"Mi Tsos", 1232);
-		
-		System.out.println(book1.getAuthor());
-		System.out.println(mitsos.getName());
+		SpringApplication.run(LibraryApplication.class, args);
+//		Book  book1 = new Book(7, "123-123-123-123", "Title7", "Author2", 80, 5);
+//		Student mitsos = new Student(1234,"Mi Tsos", 1232);
+//		System.out.println(book1.getAuthor());
+//		System.out.println(mitsos.getName());
+//		SpringApplication.run
 	}
 
 }
